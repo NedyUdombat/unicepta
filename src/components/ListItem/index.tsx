5f578226fd2dc9b59d43c7fe2b7d0c2c5a74057b
nedyudombat <nedyudombat@gmail.com>
2021-11-16 20:47:45 +0100

    Add text capitalizarion

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -18,8 +18,8 @@ const ListItem = ({ item }: ListItemProps) => {
     <>
       <Modal isOpen={isModalOpen} closeModal={() => setIsOpenModal(false)}>
         <div className="child-modal-content info">
-          <p className="title">Title: {data[0].title}</p>
-          <p className="description">{data[0].description}</p>
+          <p className="title text-capitalize">{data[0].title}</p>
+          <p className="description text-capitalize">{data[0].description}</p>
           <p className="id">Nasa ID: {data[0].nasa_id}</p>
         </div>
       </Modal>
@@ -27,7 +27,7 @@ const ListItem = ({ item }: ListItemProps) => {
         <div className="card">
           <img src={links[0].href} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{data[0].title}</h5>
+            <h5 className="card-title text-capitalize">{data[0].title}</h5>
             <button
               type="button"
               onClick={() => setIsOpenModal(true)}
